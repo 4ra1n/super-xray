a8941b8d2416cd009fe2491160e8d9aea53216c3
4ra1n <2023503307@qq.com>
2022-11-24 21:01:06 +0800

    README

---
+++ build/README.md
@@ -0,0 +1,1 @@
+Use Launch4j
--- test.bat
+++
@@ -1,4 +0,0 @@
-"C:\Program Files\Java\jdk-9.0.4\bin\java.exe" -jar super-xray-0.6-beta.jar
-"C:\Program Files\Java\jdk-17.0.3\bin\java.exe" -jar super-xray-0.6-beta.jar
-"C:\Program Files\Java\jdk-19.0.1\bin\java.exe" -jar super-xray-0.6-beta.jar
-"C:\Program Files\Java\jdk1.8.0_131\bin\java.exe" -jar super-xray-0.6-beta.jar
